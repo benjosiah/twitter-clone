1efe14a90e13a40a724660e15808fc90cc700c98
ben josiah <benjosiah90@gmail.com>
2021-01-15 12:32:42 +0100

    trending

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
-  <div class="flex text-white">
-      <side class="w-1/5"/>
+  <div class="flex h-screen w-full">
+      <side class="w-1/5 h-full border-blue-lighter border-r border-solid"/>
     <!-- tweets -->
-    <div class="tweets">
+    <div class="w-1/2 h-full">
 
 
     </div>
     <!-- explore  -->
-    <div class="explore">
-      <input type="text" placeholder="search twitter">
-      <i class="fas fa-search" ></i>
+    <div class="lg:block hidden w-1/3 h-full border-l border-solid border-grey-light py-2 px-6 relative overflow-y-scroll" >
+      <input type="text" placeholder="search twitter" class=" mb-4 rounded-full w-4/5 p-2 pl-10 bg-blue-lightest text-sm focus:outline-none">
+      <i class="fas fa-search absolute pin-t pin-l mt-5  ml-12 text-sm text-blue-light" ></i>
+      <div class=" w-full rounded-lg bg-blue-lightest">
+        <div class="flex items-center justify-between p-3">
+          <p class="font-bold text-lg ">
+            Trends for You
+          </p>
+           <i class="fas fa-cog text-lg text-blue"></i>
+          
+        </div>
+        <button v-for="trend in trending" v-bind:key="trend" class=" border-0 flex justify-between w-full hover:bg-blue-lighter border-t border-blue-lighter bg-transparent" >
+          <div>
+            <p class="text-sm text-left  leading-tight text-dark"> {{trend.top}}</p>
+            <p class="text-sm text-left  leading-tight font-bold"> {{trend.title}}</p>
+            <p class="text-sm text-left  leading-tight text-dark"> {{trend.top}}</p>
+          </div>
+          <i class="fas fa-angle-down text-lg text-dark mt-3"></i>
+        </button>
+        <button class="border-0 w-full p-3 hover:bg-blue-lighter text-left text-blue border-t bg-transparent border-blue-lighter">
+          Show more
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -41,6 +61,17 @@ export default {
         {top: 'Pop', title: 'Blue Ivy', bottom: '40k tweets'},
         {top: 'Trending in US', title: 'Denim Day', bottom: '40k tweets'},
         {top: 'Trending', title: 'When Beyonce', bottom: '25.4k tweets'},
+      ],
+        friends: [
+        {src: 'elon.jpg', name: 'Elon Musk', handle: '@teslaBoy'},
+        {src: 'monk.jpg', name: 'Adrian Monk', handle: '@detective:)'},
+        {src: 'kevin.jpg', name: 'Kevin Hart', handle: '@miniRock'}
+      ],
+      following: [
+        {src: 'elon.jpg', name: 'Elon Musk', handle: '@teslaBoy', time: '20 min', tweet: 'Should I just quarantine on mars??', comments: '1,000', retweets: '550', like: '1,000,003'},
+        {src: 'kevin.jpg', name: 'Kevin Hart', handle: '@miniRock', time: '55 min', tweet: 'Should me and the rock do another sub-par movie together????', comments: '2,030', retweets: '50', like: '20,003'},
+        {src: 'elon.jpg', name: 'Elon Musk', handle: '@teslaBoy', time: '1.4 hr', tweet: 'Haha just made a flame thrower. Shld I sell them?', comments: '100,000', retweets: '1,000,002', like: '5,000,003'},
+        {src: 'elon.jpg', name: 'Elon Musk', handle: '@teslaBoy', time: '1.4 hr', tweet: 'Just did something crazyyyyyyy', comments: '100,500', retweets: '1,000,032', like: '5,000,103'}
       ],
     }
   
@@ -81,14 +112,7 @@ export default {
     
     border:1px solid rgb(151, 173, 214);
   }
-  .fa-search{
-    position: absolute;
-    left: 0px;
-    top: 0px;
-    margin-left: 30px;
-    margin-top: 20.5px;
-   color: rgb(161, 162, 165);
-  }
+  
   .tweets{
     flex:3;
     border-right:1px solid rgb(211, 211, 208);
